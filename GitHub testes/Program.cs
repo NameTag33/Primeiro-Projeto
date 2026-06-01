f64dfc13b4b02a18e8f34ba9d9c22b574c918ddc
Lucas Rocha <2010lucasrocha@gmail.com>
2026-06-01 17:40:19 +0100

    Adiciona funcionalidade inicial de calculadora
    Foi adicionado um bloco de código no método `Main` da classe
    `Program` dentro do namespace `GitHub_testes`. Esse bloco
    implementa uma funcionalidade inicial de uma calculadora,
    incluindo:
    - Declaração da variável `Pescolha`.
    - Exibição de um título no console.
    - Opções para soma, subtração ou sair.
    - Validação de entrada do usuário com um loop `while` para
      garantir que apenas números inteiros entre 0 e 2 sejam aceitos.

--- GitHub testes/Program.cs
+++ GitHub testes/Program.cs
@@ -10,8 +10,15 @@ namespace GitHub_testes
     {
         static void Main(string[] args)
         {
+            int Pescolha;
             Console.WriteLine("======= Calculadora =======");
             Console.Write("\nDigite\n [1] Para soma\n [2] Para subtrair\n [0] Para sair\n Opção: ");
+            while(!int.TryParse(Console.ReadLine(), out Pescolha))
+            {
+                Console.WriteLine("Digite apenas numeros inteiros entr [0-2]");
+                Console.Write("Nova opção: ");
+            }
+
         }
     }
 }
